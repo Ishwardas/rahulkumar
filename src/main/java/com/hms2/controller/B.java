0e0156a16a3c03f0948726e36780d7eca9051a7f
Ihwardas <ishwar39@gmail.com>
2024-10-21 15:47:48 +0530

    kumar

--- src/main/java/com/hms2/controller/B.java
+++ src/main/java/com/hms2/controller/B.java
@@ -3,5 +3,6 @@ package com.hms2.controller;
 public class B {
     public static void main(String[] args) {
         System.out.println("helllo world");
+        System.out.println("jjjjkkkk");
     }
 }
